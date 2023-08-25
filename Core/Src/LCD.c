1737c70b3053fdf01db9b2f3244510f9ca1c80b1
Mayank <Mayankbankoti20@gmail.com>
2023-08-25 15:20:48 +0530

    LCD file created

---
+++ Core/Src/LCD.c
@@ -0,0 +1,24 @@
+/*
+ * LCD.c
+ *
+ *  Created on: Aug 25, 2023
+ *      Author: Mayank Bankoti
+ */
+
+#include "stm32f4xx_hal.h"
+/***** Define LCD Pins below **********************/
+#define RS_Pin GPIO_PIN_1
+#define RS_GPIO_PORT GPIOB
+#define RW_Pin GPIO_PIN_2
+#define RS_GPIO_PORT GPIOB
+#define EN_PIN GPIO_PIN_3
+#define EN_GPIO_PORT GPIOB
+#define D4_Pin GPIO_PIN_4
+#define GPIO_PORT GPIOB
+#define D4_Pin GPIO_PIN_5
+#define GPIO_PORT GPIOB
+#define D4_Pin GPIO_PIN_6
+#define GPIO_PORT GPIOB
+#define D4_Pin GPIO_PIN_7
+#define GPIO_PORT GPIOB
+
